b532e23a9a23b2c65aea2ba0888ae5bc9d541073
sergio arenas <sergioarenas025@gmail.com>
2022-03-23 18:42:33 -0500

    last update

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -4,6 +4,7 @@ export const InfoSection = () => {
   return (
     <div>
         <h1>TECHTRANS</h1>
+        <p>Somos una empresa</p>
     </div>
   )
 }
--- src/pages/Info/Info.jsx
+++ src/pages/Info/Info.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { Header } from '../../components/Header/Header'
-import { Footer } from '../../components/Footer/Footer'
+import { FooterFixed } from '../../components/FooterFixed/FooterFixed'
+import './Info.scss';
+import logo from '../../assets/logo4.png'
+
 
 export const Info = () => {
   return (
-    <>
+    <div className='info'>
     <Header/>
-    <Footer/>
-    </>
+    <div className='info-content'>
+      <h1>
+        Techtrans
+      </h1>
+      <p>
+      TECHTRANS S.A.S Fue fundada en el año 2022 en aras de ayudar a las microempresas y pequeñas empresas a digitalizarse, esto debido a las amplias ventajas que se encuentran en el entorno digital; ventajas tales como la ampliación del alcance de mercado, optimización del servicio, reducción de costos, entre otros; además, con un costo del servicio a un nivel asequible para empresas de estos tamaños.
+      </p>
+      <img src={logo} alt='logo'/>
+    </div>
+    <FooterFixed/>
+    </div>
   )
 }
