2ac7d473c49f5a9dbacdbac906cf10992e203ab6
sergio arenas <sergioarenas025@gmail.com>
2022-08-26 16:40:33 -0500

    feat: added the form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,32 +1,33 @@
-import React, { useRef } from 'react'
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React from 'react'
 import './ContactForm.scss'
 
 export const  ContactForm= () => {
 
-  const infoUser = [{}]
+  // const infoUser = [{}]
 
-  const selectedRef = useRef()
+  // const selectedRef = useRef()
 
-  const inputRef = useRef(null)
+  // const inputRef = useRef(null)
 
-  const handleSubmit = () => {
-    const formData = new FormData(inputRef.current)
-    const name = formData.get('name')
-    const business = formData.get('business')
-    const email = formData.get('email')
-    infoUser.push({
-      name: name,
-      business: business,
-      email:email,
-      plan: selectedRef.current.value
-    })
-    console.info(infoUser)
-  }
+  // const handleSubmit = () => {
+  //   const formData = new FormData(inputRef.current)
+  //   const name = formData.get('name')
+  //   const business = formData.get('business')
+  //   const email = formData.get('email')
+  //   infoUser.push({
+  //     name: name,
+  //     business: business,
+  //     email:email,
+  //     plan: selectedRef.current.value
+  //   })
+  //   console.info(infoUser)
+  // }
 
   return (
     <div className='contactForm'>
         <div className='contactForm-content'>
-            <h1>
+            {/* <h1>
                 Tu empresa necesita ser conocida por todos
             </h1>
             <form ref={inputRef}>
@@ -49,7 +50,8 @@ export const  ContactForm= () => {
               <option>Premium</option>
               <option>Personalizado</option>
             </select>
-            <button type='button' onClick={handleSubmit}>¡Enviamela!</button>
+            <button type='button' onClick={handleSubmit}>¡Enviamela!</button> */}
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc-9ksWhj0GUya7zisVpH6UdUcL6OT90bEVgGoTZlPEsban2Q/viewform?embedded=true" width="640" height="975" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
         </div>
     </div>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import './Header.scss'
 import React, { useRef} from 'react'
 import logo from '../../assets/logo4.png'
@@ -5,6 +6,8 @@ import { Link} from 'react-router-dom'
 
 export const Header = (services,info,contact) => {
 
+
+'https://forms.gle/ukBM7h6E4JFNGaLk7'
   const myRef=useRef()
   
   return (
@@ -18,5 +21,3 @@ export const Header = (services,info,contact) => {
     </div>
   )
 }
-
-
